85aa267ce32b59cd51d0a920930b56fffe73f499
unknown <ferdinandimeka20@gmail.com>
2024-01-30 13:26:07 +0100

    geography route

--- backend/routes/clientRoutes.js
+++ backend/routes/clientRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { getProducts, getCustomers, getTransactions } from '../controllers/clientController.js';
+import { getProducts, getCustomers, getTransactions, getGeography } from '../controllers/clientController.js';
 const router = express.Router();
 
 router.get('/products', getProducts)
 router.get('/transactions', getTransactions)
 router.get('/customers', getCustomers)
-// router.get('/geography', getGeography)
+router.get('/geography', getGeography)
 
 export default router;
